f4b3b91d9fbcb462a8683f26c56841718fa35956
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-02-06 13:14:24 +0100

    First commit

---
+++ list_methods.py
@@ -0,0 +1,19 @@
+a = [1, 2, 3]
+a.append(7)
+print(a)
+#append adds an item at the end of the list
+a.append("Bob")
+print(a)
+#string are immutable, lists are a 'container' and you can change what's inside it
+a[1] = "Alice"
+print(a)
+
+#extend is the same as append but adds another list
+a.extend(["James", "Bond"])
+print(a)
+
+#pop removes an item from position
+a.pop(4)
+print(a)
+a.remove("Bond")
+print(a)
