f1adb213bc73545eadc7520db0aef3f9cd2151cc
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-02-06 13:08:42 +0100

    First commit

---
+++ list_simple.py
@@ -0,0 +1,18 @@
+a = []
+print(a)
+a = [1, 2, 3, 4]
+print(a)
+a = [1, "james", False, None, 6.23, ["a", "b", "c"]]
+print(a)
+#in a list you can put nothing, numbers, floats, another list...
+
+a1 = [1, 2, 3, 4]
+a2 = [1, 2]
+print(a1 + a2)
+print(a2*3)
+#i can index same as string, starts from 0
+print(a1[2])
+print((a1*10)[::2]) #slicing
+#can also write this as print((a1[::2])*10)
+
+
